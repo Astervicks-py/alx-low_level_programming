01f22b720bde39db16c6e53648a7cef60f54e1be
Astervicks-py <vithomas336@gmail.com>
2022-10-15 02:27:26 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
 
 	c = 'a';
 	while
-		(c <= 'z'){
+		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
