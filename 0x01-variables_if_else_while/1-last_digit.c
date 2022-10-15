39134be54b3cb9b2b957ee000a764c5cc73a5c93
Astervicks-py <vithomas336@gmail.com>
2022-10-15 01:23:16 -0700

    Task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 		printf("Ladt digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
 	printf("Last digit of %d is %d and is 5 \n", n, m);
-	if (m < 6 && != 0)
+	if (m < 6 && m != 0)
 		printf("Lsst digit of %d is %d and is less than six and not zero", n, m);
 	/* your code goes there */
 	return (0);
