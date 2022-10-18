43dc653fc0efddc5b069afaf590b6fc65a7afcb6
Astervicks-py <vithomas336@gmail.com>
2022-10-18 15:16:17 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower letter
+ *
+ * 3-islower.c - checks if the character is in lower cases
+ */
+int_islower(int c)
+{
+	return ( c >= 'a' && c <= 'z')
+}
