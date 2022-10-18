55f645ee8a494a9e48d092abedf073a6d7f957f0
Astervicks-py <vithomas336@gmail.com>
2022-10-18 15:44:38 -0700

    TRask

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - print a bunch of stuff that i am too lazy type
+ * @c: also print things
+ * Return: returns stuff
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
