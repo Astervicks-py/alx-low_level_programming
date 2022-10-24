49d9ef2836582ec522f944de27de4d9302b33479
Astervicks-py <vithomas336@gmail.com>
2022-10-24 15:01:12 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - returns the length of the string
+ * @a: int a
+ * @b: int b
+ * Return: Success 1;
+ */
+void swap_int(int *a, int *b);
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
